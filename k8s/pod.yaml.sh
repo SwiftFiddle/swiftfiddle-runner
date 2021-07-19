284008453518a8a43e1aa5d102c904bf34cbd8be
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2021-07-19 18:50:25 +0900

    timestamp

--- k8s/pod.yaml.sh
+++ k8s/pod.yaml.sh
@@ -4,9 +4,12 @@ set -e
 CWD=$(cd $(dirname $0); pwd)
 rm -f "$CWD/pod.yaml"
 
+commit_sha=$(git rev-parse HEAD)
+timestamp=$1
+
 versions=($(cat $CWD/../versions.txt | tr "\n" " "))
 for version in "${versions[@]}"; do
   name="runner-v$(sed 's/\.//g' <<<"$version")"
-  commit_sha=$(git rev-parse HEAD)
-  sed "s/%NAME%/$name/g;s/%IMAGE%/swiftfiddle\/runner:$version/g;s/%COMMIT_SHA%/$commit_sha/g;" "$CWD/pod_template.yaml" >> "$CWD/pod.yaml"
+  sed "s/%NAME%/$name/g;s/%IMAGE%/swiftfiddle\/runner:$version/g;s/%COMMIT_SHA%/$commit_sha/g;s/%TIMESTAMP%/$timestamp/g;" \
+      "$CWD/pod_template.yaml" >> "$CWD/pod.yaml"
 done
