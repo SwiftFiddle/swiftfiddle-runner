7e5007cf7f62c0b7265103276bbac01c33d3efb0
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-08-09 00:10:01 +0900

    Refine deploy script

--- k8s/pod.yaml.sh
+++ k8s/pod.yaml.sh
@@ -2,17 +2,11 @@
 set -e
 
 CWD=$(cd $(dirname $0); pwd)
-rm -f "$CWD/pod.yaml"
 
 commit_sha=$(git rev-parse HEAD)
-timestamp=$1
+version=$1
+name=$2
+timestamp=$3
 
-versions=($(cat $CWD/../versions.txt | tr "\n" " "))
-for versionGroup in "${versions[@]}"; do
-  read -r -a group <<< $(echo $versionGroup | sed "s/,/ /g")
-  version=${group[${#group[@]}-1]}
-  name="runner-v$(sed 's/\.//g' <<<"$version")"
-
-  sed "s/%NAME%/$name/g;s/%IMAGE%/swiftfiddle\/runner:$version/g;s/%COMMIT_SHA%/$commit_sha/g;s/%TIMESTAMP%/$timestamp/g;" \
-    "$CWD/pod_template.yaml" >> "$CWD/pod.yaml"
-done
+sed "s/%NAME%/$name/g;s/%IMAGE%/swiftfiddle\/runner:$version/g;s/%COMMIT_SHA%/$commit_sha/g;s/%TIMESTAMP%/$timestamp/g;" \
+  "$CWD/pod_template.yaml" >> "$CWD/pod.yaml"
