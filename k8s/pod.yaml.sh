1c5b216f4e987f39cd69867dfa7fb8c1254d19fa
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-08-18 15:41:55 +0900

    Refactor

--- k8s/pod.yaml.sh
+++ k8s/pod.yaml.sh
@@ -6,9 +6,8 @@ CWD=$(cd $(dirname $0); pwd)
 commit_sha=$(git rev-parse HEAD)
 version=$1
 name=$2
-replicas=$3
-timestamp=$4
+timestamp=$3
 image="us.gcr.io\/swift-playground-fbe87\/runner:$version"
 
-sed "s/%NAME%/$name/g;s/%IMAGE%/$image/g;s/%VERSION%/$version/g;s/%REPLICAS%/$replicas/g;s/%COMMIT_SHA%/$commit_sha/g;s/%TIMESTAMP%/$timestamp/g;" \
+sed "s/%NAME%/$name/g;s/%IMAGE%/$image/g;s/%VERSION%/$version/g;s/%COMMIT_SHA%/$commit_sha/g;s/%TIMESTAMP%/$timestamp/g;" \
   "$CWD/pod_template.yaml" >> "$CWD/pod.yaml"
