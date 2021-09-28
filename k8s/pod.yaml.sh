987719e4fc101fcb91074b378f49e8f00464cca7
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-09-28 10:04:58 +0900

    update min-replicas

--- k8s/pod.yaml.sh
+++ k8s/pod.yaml.sh
@@ -7,7 +7,6 @@ commit_sha=$(git rev-parse HEAD)
 version=$1
 name=$2
 image="swiftfiddle.azurecr.io\/swiftfiddle\/runner:$version"
-replicas=$3
 
-sed "s/%NAME%/$name/g;s/%IMAGE%/$image/g;s/%VERSION%/$version/g;s/%MIN_REPLICAS%/$replicas/g;s/%COMMIT_SHA%/$commit_sha/g;" \
+sed "s/%NAME%/$name/g;s/%IMAGE%/$image/g;s/%VERSION%/$version/g;s/%COMMIT_SHA%/$commit_sha/g;" \
   "$CWD/pod_template.yaml"
