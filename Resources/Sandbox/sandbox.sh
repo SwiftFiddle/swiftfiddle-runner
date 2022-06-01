b2270f987f98160fdda3d8e4151fd8568c8a2408
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2022-06-02 00:51:47 +0900

    128 MB memory limit, 10% CPU usage

--- Resources/Sandbox/sandbox.sh
+++ Resources/Sandbox/sandbox.sh
@@ -11,9 +11,9 @@ fi
 
 # 10MB file size limit
 # 20 processes limit
-# 256 MB memory limit
-# 20% CPU usage
-containerId=$(docker run --pull never --env _COLOR=$_COLOR --rm --detach --ulimit fsize=10000000:10000000 --pids-limit 20 --memory 256m --cpus="0.2" "$@")
+# 128 MB memory limit
+# 10% CPU usage
+containerId=$(docker run --pull never --env _COLOR=$_COLOR --rm --detach --ulimit fsize=10000000:10000000 --pids-limit 20 --memory 128m --cpus="0.1" "$@")
 status=$($timeoutCommand "$to" docker wait "$containerId" || true)
 docker kill $containerId &> /dev/null
 
