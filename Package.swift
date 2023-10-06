921353452dae99eb490755ff0b29605b33d7573c
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2023-10-06 10:37:40 +0900

    Code Format

--- Package.swift
+++ Package.swift
@@ -1,24 +1,24 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "swiftfiddle-runner",
-    platforms: [
-        .macOS(.v12)
-    ],
-    dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.84.4"),
-    ],
-    targets: [
-        .target(
-            name: "App",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
-            ]
-        ),
-        .executableTarget(name: "Run", dependencies: [.target(name: "App")])
-    ]
+  name: "swiftfiddle-runner",
+  platforms: [
+    .macOS(.v12)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.84.4"),
+  ],
+  targets: [
+    .target(
+      name: "App",
+      dependencies: [
+        .product(name: "Vapor", package: "vapor"),
+      ],
+      swiftSettings: [
+        .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+      ]
+    ),
+    .executableTarget(name: "Run", dependencies: [.target(name: "App")])
+  ]
 )
